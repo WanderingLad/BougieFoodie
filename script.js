a54e33655dd0058a87ed6cc08cbd0a0d8a7c8bd7
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-02-10 22:22:06 -0500

    previous search js with a few ids in html

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-var apiKey ='AIzaSyB84GbD7RpiItTnqRXSgGmgQdDJOOYUx8M';
+var apiKey = 'AIzaSyB84GbD7RpiItTnqRXSgGmgQdDJOOYUx8M';
 var search = document.getElementById("btn");
 var input = document.getElementById("input");
 
-function getApi() {
-    var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q='+input.value+'&key=AIzaSyB84GbD7RpiItTnqRXSgGmgQdDJOOYUx8M'
-    fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
-}  
-search.addEventListener('click',getApi);
+// function getApi() {
+//     var url = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&q='+input.value+'&key=AIzaSyB84GbD7RpiItTnqRXSgGmgQdDJOOYUx8M'
+//     fetch(url)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data);
+//     });
+// }  
+// search.addEventListener('click',getApi);
+
+
+//-------
+
+// When user searches in main bar, I need to grab value
+var storeItem = localStorage.getItem("history");//JSON.parse (return)
+storeItem = JSON.parse(storeItem)
+var results = [];
+search.onclick = savedResults;
+
+function savedResults() {
+  var saveValue = input.value;
+  results.push(saveValue);
+  localStorage.setItem("history", JSON.stringify(results));
+  //JSON.stringify results
+}
+// Needs to save to previous search section
+// I want all the values to remain on the page
